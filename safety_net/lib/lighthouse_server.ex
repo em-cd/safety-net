9e06e410ee9c96eccfef57ba40eac896141c03d4
Cpales <paraloopable@gmail.com>
2025-11-20 15:15:32 +0100

    the LHS keeps a list of ships and can print a report

--- safety_net/lib/lighthouse_server.ex
+++ safety_net/lib/lighthouse_server.ex
@@ -1,30 +1,42 @@
 defmodule LighthouseServer do
+  # "LighthouseServer" might be written as "LHS" in the comments
   use GenServer
 
-  def start_link() do
+  def start_link do
     IO.puts("Lighthouse starting...")
-    GenServer.start_link(LighthouseServer,%{}, name: :lighthouse)
+    GenServer.start_link(LighthouseServer,[], name: :lighthouse)
   end
 
-  def add_ship(id, position, liveness) do
-  GenServer.cast(:lighthouse, {:add_ship, %{id: id, coords: position,status: liveness}})
+  def add_ship(ship) do
+    #at spawn, each ship has to run this to be added to the LHS
+    GenServer.cast(:lighthouse, {:add_ship, ship})
   end
+
+  def report, do: GenServer.call(:lighthouse, :report)
 
   @impl true
   def init(_state) do
-    {:ok, %{id: nil, coords: {}, status: :nil}}
+    # The initial_state is an empty list
+    # in which maps (id/coords/status) are added.
+    # This way, each map identifies a ship and its state,
+    # whilest the LHS-list will effectively represent the fleet's states
+
+    # %{id: id, coords: {x, y}, status: :nil} <- SHIP'S STATE
+    # [ship1, ship2...] <- LHS state
+
+    {:ok, []}
   end
 
   # handle_cast receives from the fleet and sends to
   @impl true
-  def handle_cast({ship_id, coordinates, liveness}, _state) do
-    new_state = %{id: ship_id, coords: coordinates, status: liveness}
+  def handle_cast({:add_ship, ship}, state) do
+    new_state = [ship | state]
     {:noreply, new_state}
   end
 
   @impl true
   def handle_call(:report, _from, state) do
-
-    {:reply, state}
+    fleet = state
+    {:reply, fleet, state}
   end
 end
