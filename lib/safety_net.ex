40cb1ba95681620e6ecfb4adb3ab9ab0c5ed2ad9
elizabeth cullen davison <lizzie.cullendavison@gmail.com>
2025-11-27 10:35:07 +0100

    Send ping requests

--- lib/safety_net.ex
+++ lib/safety_net.ex
@@ -10,33 +10,67 @@ def start() do
     pid
   end
 
-  def handler do
+  def handler() do
     receive do
       {:init} ->
         IO.puts("#{inspect(self())}: received init")
         wait_for_nodes()
         spawn(fn -> ping_scheduler() end)
-        handler()
+        handler(Map.new())
+    end
+  end
 
+  # missing_acks is a map of the form: %{dest_node: ack_requester}
+  # where dest_node is the node that we pinged, and ack_requester is the node who should receive
+  # an ack. This is either the current node, or another node that requested a ping.
+  def handler(missing_acks) do
+    IO.puts("#{inspect(self())}: handler, awaiting acks: #{inspect(missing_acks)}")
+
+    receive do
       {:ping, src_node} ->
         IO.puts("#{inspect(self())}: received ping from #{inspect(src_node)}, sending ack")
-        send({:handler, src_node}, {:ack, Node.self()})
-        handler()
+        send_remote(src_node, :ack)
+        handler(missing_acks)
+
+      {:ping_sent, dest_node} ->
+        handler(Map.put(missing_acks, dest_node, :me))
 
       {:ack, src_node} ->
         IO.puts("#{inspect(self())}: received ack from #{inspect(src_node)}")
-        handler()
+        ack_requester = Map.fetch(missing_acks, src_node)
+        if ack_requester != :me do
+          send_remote(ack_requester, :ack, src_node)
+        end
+        handler(Map.delete(missing_acks, src_node))
+
+      {:ping_request, src_node, node} ->
+        send_remote(node, :ping)
+        new_missing_acks = Map.put(missing_acks, node, src_node)
+        handler(new_missing_acks)
     end
   end
 
   def ping_scheduler() do
-    Enum.each(Enum.shuffle(Node.list()), fn p ->
+    Enum.each(Enum.shuffle(Node.list()), fn n ->
+      IO.puts("#{inspect(self())}: sending ping to #{inspect(n)}")
+      send_remote(n, :ping)
+      send(:handler, {:ping_sent, n})
       :timer.sleep(5_000)
-      IO.puts("#{inspect(self())}: sending ping to #{inspect(p)}")
-      send({:handler, p}, {:ping, Node.self()})
     end)
 
     ping_scheduler()
+  end
+
+  def send_remote(dest_node, msg_type, src_node \\ Node.self())
+  def send_remote(dest_node, msg_type, src_node) do
+    send({:handler, dest_node}, {msg_type, src_node})
+  end
+
+  def send_ping_requests(node) do
+    other_nodes = List.delete(Node.list(), node)
+    Enum.each(other_nodes, fn n ->
+      send({:handler, n}, {:ping_request, Node.self(), node})
+    end)
   end
 
   def wait_for_nodes() do
--- test/safety_net_test.exs
+++ test/safety_net_test.exs
@@ -8,4 +8,11 @@ defmodule SafetyNetTest do
     assert {:init} in messages
   end
 
+  test "handler/1, ping" do
+    node = node()
+    Process.register(self(), :handler)
+    pid = spawn(fn -> SafetyNet.handler(Map.new()) end)
+    send(pid, {:ping, node})
+    assert_receive({:ack, ^node})
+  end
 end
