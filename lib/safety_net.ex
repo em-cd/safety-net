6d2a2b53d56d69e598a50fac98a70072cda39477
elizabeth cullen davison <lizzie.cullendavison@gmail.com>
2025-11-28 11:24:18 +0100

    WIP: share own status updates with pings

--- lib/safety_net.ex
+++ lib/safety_net.ex
@@ -4,24 +4,45 @@ defmodule SafetyNet do
   """
   use GenServer
 
-  defstruct [:id, :peers, :coords, :status]
+  defstruct [:id, :peers, :coords]
 
-  def start_link(id, peers \\ [], coords \\ {0, 0}, status \\ :alive) do
-    GenServer.start_link(__MODULE__, {id, peers, coords, status}, name: via(id))
+  def start_link(id, peers \\ [], coords \\ {0, 0}) do
+    GenServer.start_link(__MODULE__, {id, peers, coords}, name: via(id))
   end
 
   defp via(id), do: {:via, Registry, {SafetyNet, id}}
 
+  # State should end up looking like:
+  # state = %{
+  #   id: id,
+  #   coords: coords,
+  #   incarnation: num,
+  #   peers: %{
+  #     :node_id =>
+  #       %{
+  #         coords: last_known_coords,
+  #         status: last_known_status,
+  #         incarnation: last_known_incarnation
+  #       },
+  #     ...
+  #   }
+  # }
   @impl true
-  def init({id, peers, coords, status}) do
+  def init({id, peers, coords}) do
+    peer_map =
+      peers
+      |> Enum.map(fn peer_id ->
+        {peer_id, %{status: :alive, coords: nil}}
+      end)
+      |> Enum.into(%{})
+
     state = %__MODULE__{
       id: id,
-      peers: peers,
-      coords: coords,
-      status: status
+      peers: peer_map,
+      coords: coords
     }
 
-    LighthouseServer.add_ship(state)
+    LighthouseServer.add_ship(own_membership(state))
     schedule_probe()
     time_to_move()
 
@@ -29,11 +50,20 @@ defmodule SafetyNet do
   end
 
   @impl true
+  def handle_cast({:ping, from_id, from_pid, _gossip}, state) do
+    IO.puts("#{state.id}: received ping from #{from_id}, sending ack")
+    send(from_pid, {:ack, state.id})
+    {:noreply, state}
+  end
+
+  @impl true
   def handle_info(:probe, state) do
     # Pick a random peer and ping them
-    if state.peers != [] do
-      peer = Enum.random(state.peers)
-      GenServer.cast(via(peer), {:ping, state.id, self()})
+    # TODO: only pick alive nodes to ping
+    if state.peers do
+      {peer_id, _peer_data} = Enum.random(state.peers)
+      gossip = prepare_gossip(state)
+      GenServer.cast(via(peer_id), {:ping, state.id, self(), gossip})
     end
 
     # Reschedule the probe
@@ -62,18 +92,24 @@ defmodule SafetyNet do
     {:noreply, state}
   end
 
-  @impl true
-  def handle_cast({:ping, from_id, from_pid}, state) do
-    IO.puts("#{state.id}: received ping from #{from_id}, sending ack")
-    send(from_pid, {:ack, state.id})
-    {:noreply, state}
-  end
-
   defp schedule_probe do
     Process.send_after(self(), :probe, 5000)
   end
 
   defp time_to_move do
     Process.send_after(self(), :chopchop, 5_000)
+  end
+
+  defp prepare_gossip(state) do
+    # TODO: pick some peer's updates to share as well as own status
+    [own_membership(state)]
+  end
+
+  defp own_membership(state) do
+    %{
+      id: state.id,
+      coords: state.coords,
+      status: :alive
+    }
   end
 end
