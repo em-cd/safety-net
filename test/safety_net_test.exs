867f29ca327ad100ee6ac950d0b9fc20ddf99a6f
elizabeth cullen davison <lizzie.cullendavison@gmail.com>
2025-11-28 10:01:41 +0100

    Remove old tests

--- test/safety_net_test.exs
+++ test/safety_net_test.exs
@@ -2,17 +2,4 @@ defmodule SafetyNetTest do
   use ExUnit.Case
   doctest SafetyNet
 
-  test "start/0" do
-    pid = SafetyNet.start()
-    {:messages, messages} = Process.info(pid, :messages)
-    assert {:init} in messages
-  end
-
-  test "handler/1, ping" do
-    node = node()
-    Process.register(self(), :handler)
-    pid = spawn(fn -> SafetyNet.handler(Map.new()) end)
-    send(pid, {:ping, node})
-    assert_receive({:ack, ^node})
-  end
 end
